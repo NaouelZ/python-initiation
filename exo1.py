5cdb40c21fdbb57be797a29f29bea23b78f251c7
Naouel <zenati.naouel14@gmail.com>
2020-09-29 10:54:32 +0200

    exo

--- exo1.py
+++ exo1.py
@@ -1,7 +1,7 @@
 def ThreeDigit () :
     for i in range(999):
         l = list(str(f'{i:03}'))
-        if l[0] != l[1] and l[1] != l[2] and l[1] != l[0] :
+        if l[0] != l[1] and l[1] != l[2] and l[2] != l[0] :
             print(f'{i:03}')
         
 ThreeDigit()
