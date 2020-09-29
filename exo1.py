264ee51a0a410f6a5fbb08c8b066123f339778ea
Naouel <zenati.naouel14@gmail.com>
2020-09-29 11:16:42 +0200

    correction

--- exo1.py
+++ exo1.py
@@ -1,7 +1,12 @@
+displayed = []
+
 def ThreeDigit () :
     for i in range(999):
         l = list(str(f'{i:03}'))
-        if l[0] != l[1] and l[1] != l[2] and l[2] != l[0] :
-            print(f'{i:03}')
+        number = f'{i:03}'
+        if number not in displayed:
+            displayed.append(number)
+            if l[0] != l[1] and l[1] != l[2] and l[2] != l[0] :
+                print(number)        
         
 ThreeDigit()
